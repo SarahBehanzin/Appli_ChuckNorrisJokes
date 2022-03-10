e477c0126e89c27cf89f8112b29be99aedff8a8b
AmineBelgacem <aminebelgacem00@gmail.com>
2022-03-10 10:45:07 +0100

    Initialisation application

---
+++ app/src/main/java/com/example/appli_belgacem_behanzin/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.appli_belgacem_behanzin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
