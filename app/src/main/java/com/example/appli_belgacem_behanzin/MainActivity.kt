a1e8963bf9686d53663ba5a2afcc83fd0958c6da
“Behanzin <sarah.behanzin@edu.esiee.fr>
2022-03-14 18:38:00 +0100

    Liste des blagues

--- app/src/main/java/com/example/appli_belgacem_behanzin/MainActivity.kt
+++ app/src/main/java/com/example/appli_belgacem_behanzin/MainActivity.kt
@@ -3,11 +3,10 @@ package com.example.appli_belgacem_behanzin
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val names=listOf("Chuck Norris knows exactly how many licks it takes to reach the center of a tootsipop.", "Chuck Norris can make a happy meal cry", "Chuck Norris once made his Kindergarten teacher spit out her gum.", "i dont know why people treat Chuck Norris like hes a killer he has the heart of a child which isa bad thing", "Many people wonder why Star Wars begins with \\\"A long time ago, in a galaxy far, far away\\\"... Well, Chuck Norris roundhouse kicked all the jedi, clones & other aliens in that galaxy and moved to ours.", "Chuck Norris is the reason that the Army had to change their slogan, \\\"An Army of One\\\".", "The USA is now the CNC [Chuck Norris Country]. Lawl did you really think i was Chuck Norris? no, im that \\\"...\\\" guy trololololololoftjhdlshkgksejeklnadjsuehukdkjfdlxgijhjwvddkDARN NOT AGAINhgjfsncdjcndchcnuckjhkxzsgk", "Chuck Norris is so passionate, all of his crimes of passion are misdemenor fines that all of his victims/chosen ones will gladly pay for him.", "Chuck Norris once got into a pissing match with a fire hose and won!", "Chuck Norris can reverse Convection.")
     }
 }
