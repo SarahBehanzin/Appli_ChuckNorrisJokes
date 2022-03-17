c4f63ca25094ea50c44b05944de5d3d90bcc49ad
“Behanzin <sarah.behanzin@edu.esiee.fr>
2022-03-17 09:44:53 +0100

    2. RecyclerView instantiation

--- app/src/main/java/com/example/appli_belgacem_behanzin/MainActivity.kt
+++ app/src/main/java/com/example/appli_belgacem_behanzin/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.appli_belgacem_behanzin
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 
 class MainActivity : AppCompatActivity() {
@@ -11,5 +13,8 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         Log.i("list_jokes", ListJokes.mocked.toString())
+
+        val myRV=findViewById<RecyclerView>(R.id.recyclerView)
+        myRV.layoutManager = LinearLayoutManager(this)
     }
 }
