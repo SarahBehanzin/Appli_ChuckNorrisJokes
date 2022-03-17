7b136e7509ffc3b891434e6f9a27c1a085c806fe
“Behanzin <sarah.behanzin@edu.esiee.fr>
2022-03-17 08:23:30 +0100

    oui

--- app/src/main/java/com/example/appli_belgacem_behanzin/MainActivity.kt
+++ app/src/main/java/com/example/appli_belgacem_behanzin/MainActivity.kt
@@ -2,11 +2,14 @@ package com.example.appli_belgacem_behanzin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Log.i("list_jokes","liste")
     }
 }
