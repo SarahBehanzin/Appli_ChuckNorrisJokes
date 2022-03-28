690ea8f57d0ea25f0aef315f60a4cabd215635cd
“Behanzin <sarah.behanzin@edu.esiee.fr>
2022-03-28 17:52:34 +0200

    4. Custom view for items

--- app/src/main/java/com/example/appli_belgacem_behanzin/JokeAdapter.kt
+++ app/src/main/java/com/example/appli_belgacem_behanzin/JokeAdapter.kt
@@ -1,26 +1,31 @@
 package com.example.appli_belgacem_behanzin
 
-import android.text.Layout
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.recyclerview.widget.RecyclerView
 import com.example.appli_belgacem_behanzin.ListJokes.mocked
 
 
-class JokeAdapter : RecyclerView.Adapter<JokeAdapter.JokeViewHolder>(){
-    inner class JokeViewHolder(val jokeTextView:TextView) : RecyclerView.ViewHolder(jokeTextView)
+class JokeAdapter : RecyclerView.Adapter<JokeAdapter.JokeViewHolder>() {
+    inner class JokeViewHolder(val constraintLayout: ConstraintLayout) :
+        RecyclerView.ViewHolder(constraintLayout)
 
     //créer la view holder
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): JokeAdapter.JokeViewHolder {
-        val view= LayoutInflater.from(parent.context).inflate(R.layout.joke_layout, parent,false)
+        val view: ConstraintLayout = LayoutInflater.from(parent.context)
+            .inflate(R.layout.joke_layout, parent, false) as ConstraintLayout
         //return JokeViewHolder(TextView(parent.context))
-        return JokeViewHolder(view as TextView)
+        return JokeViewHolder(view)
     }
 
     //utilsée pour en gros la prochaine view quand l'autre part
     override fun onBindViewHolder(holder: JokeAdapter.JokeViewHolder, position: Int) {
-        holder.jokeTextView.text= mocked[position]
+        val textJoke = holder.constraintLayout.findViewById<TextView>(R.id.textView)
+        val m: String = mocked[position]
+        textJoke.text = m
+        //holder.constraintLayout.R.id.textView= mocked[position]
     }
 
     override fun getItemCount() = mocked.size
