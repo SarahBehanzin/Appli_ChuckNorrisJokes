0a2ead103137dd4bf46f26039b7f2123a9aee6bf
“Behanzin <sarah.behanzin@edu.esiee.fr>
2022-03-28 18:04:44 +0200

    1.4. Custom view for items

--- app/src/main/java/com/example/appli_belgacem_behanzin/JokeAdapter.kt
+++ app/src/main/java/com/example/appli_belgacem_behanzin/JokeAdapter.kt
@@ -10,22 +10,22 @@ import com.example.appli_belgacem_behanzin.ListJokes.mocked
 
 class JokeAdapter : RecyclerView.Adapter<JokeAdapter.JokeViewHolder>() {
     inner class JokeViewHolder(val constraintLayout: ConstraintLayout) :
-        RecyclerView.ViewHolder(constraintLayout)
+        RecyclerView.ViewHolder(constraintLayout) {
+           fun updateText(text:String){
+               constraintLayout.findViewById<TextView>(R.id.textView).text=text
+           }
+    }
 
     //créer la view holder
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): JokeAdapter.JokeViewHolder {
         val view: ConstraintLayout = LayoutInflater.from(parent.context)
             .inflate(R.layout.joke_layout, parent, false) as ConstraintLayout
-        //return JokeViewHolder(TextView(parent.context))
         return JokeViewHolder(view)
     }
 
     //utilsée pour en gros la prochaine view quand l'autre part
     override fun onBindViewHolder(holder: JokeAdapter.JokeViewHolder, position: Int) {
-        val textJoke = holder.constraintLayout.findViewById<TextView>(R.id.textView)
-        val m: String = mocked[position]
-        textJoke.text = m
-        //holder.constraintLayout.R.id.textView= mocked[position]
+        holder.updateText(mocked[position])
     }
 
     override fun getItemCount() = mocked.size
