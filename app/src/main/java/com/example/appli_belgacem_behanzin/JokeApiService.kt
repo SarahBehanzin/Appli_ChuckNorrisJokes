e07eebb9b4093541fdc43eff612b0d939dd7a20f
“Behanzin <sarah.behanzin@edu.esiee.fr>
2022-04-04 18:50:12 +0200

    2.3. Retrofit usage

--- app/src/main/java/com/example/appli_belgacem_behanzin/JokeApiService.kt
+++ app/src/main/java/com/example/appli_belgacem_behanzin/JokeApiService.kt
@@ -1,9 +1,9 @@
 package com.example.appli_belgacem_behanzin
 import com.example.appli_belgacem_behanzin.ListJokes.mocked
+import io.reactivex.Single
 import retrofit2.http.GET
 
 interface JokeApiService {
-    @GET("url_path_extension") fun giveMeAJoke(liste:List<Joke>): Joke {
-        return liste.single()
-    }
+    @GET("/random")
+    fun giveMeAJoke(): Single<Joke>
 }
--- app/src/main/java/com/example/appli_belgacem_behanzin/JokeApiServiceFactory.kt
+++ app/src/main/java/com/example/appli_belgacem_behanzin/JokeApiServiceFactory.kt
@@ -4,11 +4,25 @@ import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFact
 import kotlinx.serialization.json.Json
 import okhttp3.MediaType
 import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.create
 
+//création de l'objet Factory
 object JokeApiServiceFactory {
-    fun function_api(joke:JokeApiService):JokeApiService{
-        val builder=Retrofit.Builder()
-            Json.asConverterFactory(MediaType.get("application/json"))
-        return joke
-    }
+    fun function_api():JokeApiService=
+        Retrofit.Builder()
+            .baseUrl("https://api.chucknorris.io/jokes")//on donne la base url
+            //permet de convertir en Json
+            .addConverterFactory(Json.asConverterFactory(MediaType.get("application/json")))
+            //permet d'adapter la Factory avec la fonction create(fonction statique)
+            .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+            //on build
+            .build()
+            //permet de retourner l'instance
+            .create(JokeApiService::class.java)
+}
+
+fun main(){
+    val v_service:JokeApiService=JokeApiServiceFactory.function_api()
+    val single_joke=v_service.giveMeAJoke()
 }
