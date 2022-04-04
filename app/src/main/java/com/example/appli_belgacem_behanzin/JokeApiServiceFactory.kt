642e964e28d5eb8389083957df078d311dbf12f0
“Behanzin <sarah.behanzin@edu.esiee.fr>
2022-04-04 12:31:00 +0200

    2.3. Retrofit usage

--- app/src/main/java/com/example/appli_belgacem_behanzin/JokeApiServiceFactory.kt
+++ app/src/main/java/com/example/appli_belgacem_behanzin/JokeApiServiceFactory.kt
@@ -1,9 +1,14 @@
 package com.example.appli_belgacem_behanzin
 
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+import kotlinx.serialization.json.Json
+import okhttp3.MediaType
 import retrofit2.Retrofit
 
 object JokeApiServiceFactory {
-    fun function_api():JokeApiService{
-        return ???
+    fun function_api(joke:JokeApiService):JokeApiService{
+        val builder=Retrofit.Builder()
+            Json.asConverterFactory(MediaType.get("application/json"))
+        return joke
     }
 }
