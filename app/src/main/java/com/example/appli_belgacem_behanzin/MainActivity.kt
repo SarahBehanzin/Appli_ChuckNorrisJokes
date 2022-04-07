8f3bc37afb7af954198ae9d80d9c74fe7f8a4bbf
“Behanzin <sarah.behanzin@edu.esiee.fr>
2022-04-07 15:17:11 +0200

    2.5. Leaks killer

--- app/src/main/java/com/example/appli_belgacem_behanzin/MainActivity.kt
+++ app/src/main/java/com/example/appli_belgacem_behanzin/MainActivity.kt
@@ -5,15 +5,27 @@ import android.os.Bundle
 import android.util.Log
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.disposables.Disposable
 import io.reactivex.rxkotlin.subscribeBy
 import io.reactivex.schedulers.Schedulers
 
 class MainActivity : AppCompatActivity() {
+    val tag="mainActivity"
+    val compositeDisposable = CompositeDisposable()
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.i(tag,"composite avant le clear ${compositeDisposable.size()}")
+        compositeDisposable.clear()
+        Log.i(tag,"composite après le clear ${compositeDisposable.size()}")
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        Log.i("list_jokes", ListJokes.mocked.toString())
+        Log.i(tag, ListJokes.mocked.toString())
 
         val myRV=findViewById<RecyclerView>(R.id.recyclerView)
         myRV.layoutManager = LinearLayoutManager(this)
@@ -22,12 +34,13 @@ class MainActivity : AppCompatActivity() {
 
         val myService:JokeApiService=JokeApiServiceFactory.function_api()
 
-        myService.giveMeAJoke()
+        val jokeDisposable: Disposable =myService.giveMeAJoke()
             .subscribeOn(Schedulers.io())
             .subscribeBy(
-            onError = {Log.e("Erreur", "La Joke n'a pas été reçue.", it)},
-            onSuccess={Log.i("Joke reçue", it.toString())}
-        )
-
+                onError = {Log.e(tag, "La Joke n'a pas été reçue.", it)},
+                onSuccess={Log.i(tag, it.toString())}
+            )
+        //ajout du jokeDisposable dans le compositeDisposable
+        compositeDisposable.add(jokeDisposable)
     }
 }
